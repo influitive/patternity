0f772b7d82041f7615b780f7ca219a06345ccee2
Nick Faulkner <nick.faulkner@influitive.com>
2015-03-05 14:08:29 -0500

    fixed accordion not working in patternlab

--- infl-patternlab/source/js/application.js
+++ infl-patternlab/source/js/application.js
@@ -1,1 +1,6 @@
-window.Accordion = require("../../infl-components/accordion.jsx");
+var inflComp = {};
+
+inflComp.React = require('react');
+inflComp.Accordion = require("../../infl-components/accordion.jsx");
+
+module.exports = inflComp;
--- infl-patternlab/source/js/infl-components.js
+++ infl-patternlab/source/js/infl-components.js
@@ -1,8 +1,13 @@
-(function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);var f=new Error("Cannot find module '"+o+"'");throw f.code="MODULE_NOT_FOUND",f}var l=n[o]={exports:{}};t[o][0].call(l.exports,function(e){var n=t[o][1][e];return s(n?n:e)},l,l.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({"./source/js/application.js":[function(require,module,exports){
-window.Accordion = require("../../infl-components/accordion.jsx");
+!function(e){if("object"==typeof exports&&"undefined"!=typeof module)module.exports=e();else if("function"==typeof define&&define.amd)define([],e);else{var f;"undefined"!=typeof window?f=window:"undefined"!=typeof global?f=global:"undefined"!=typeof self&&(f=self),f.inflComp=e()}}(function(){var define,module,exports;return (function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);var f=new Error("Cannot find module '"+o+"'");throw f.code="MODULE_NOT_FOUND",f}var l=n[o]={exports:{}};t[o][0].call(l.exports,function(e){var n=t[o][1][e];return s(n?n:e)},l,l.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({"./source/js/application.js":[function(require,module,exports){
+var inflComp = {};
+
+inflComp.React = require('react');
+inflComp.Accordion = require("../../infl-components/accordion.jsx");
+
+module.exports = inflComp;
 
 
-},{"../../infl-components/accordion.jsx":"/Users/nickfaulkner/Code/infl/patternity/infl-patternlab/infl-components/accordion.jsx"}],"/Users/nickfaulkner/Code/infl/patternity/infl-patternlab/infl-components/accordion.jsx":[function(require,module,exports){
+},{"../../infl-components/accordion.jsx":"/Users/nickfaulkner/Code/infl/patternity/infl-patternlab/infl-components/accordion.jsx","react":"/Users/nickfaulkner/Code/infl/patternity/node_modules/react/react.js"}],"/Users/nickfaulkner/Code/infl/patternity/infl-patternlab/infl-components/accordion.jsx":[function(require,module,exports){
 var React = require('react/addons');
 var classNames = require('classnames');
 
@@ -72,7 +77,6 @@ var AccordionHeader = React.createClass({displayName: "AccordionHeader",
     isEnabled: React.PropTypes.bool
   },
   _toggleContent: function(){
-    alert("test");
     if(this.props.isEnabled){
       this.props.toggleOne(this.props.index);
     }
@@ -20141,4 +20145,8 @@ if ("production" !== process.env.NODE_ENV) {
 module.exports = warning;
 
 }).call(this,require('_process'))
-},{"./emptyFunction":"/Users/nickfaulkner/Code/infl/patternity/node_modules/react/lib/emptyFunction.js","_process":"/Users/nickfaulkner/Code/infl/patternity/node_modules/browserify/node_modules/process/browser.js"}]},{},["./source/js/application.js"]);
+},{"./emptyFunction":"/Users/nickfaulkner/Code/infl/patternity/node_modules/react/lib/emptyFunction.js","_process":"/Users/nickfaulkner/Code/infl/patternity/node_modules/browserify/node_modules/process/browser.js"}],"/Users/nickfaulkner/Code/infl/patternity/node_modules/react/react.js":[function(require,module,exports){
+module.exports = require('./lib/React');
+
+},{"./lib/React":"/Users/nickfaulkner/Code/infl/patternity/node_modules/react/lib/React.js"}]},{},["./source/js/application.js"])("./source/js/application.js")
+});
