9ed3c7aa0cf1bf5df5922b014252a51000311b50
Nick Faulkner <nick.faulkner@influitive.com>
2015-03-25 15:29:02 -0400

    updated select dropdown to include selcted

--- infl-components/select_dropdown.jsx
+++ infl-components/select_dropdown.jsx
@@ -3,7 +3,6 @@ var React = require('react');
 var SelectDropdown = React.createClass({
   getDefaultProps: function() {
     return {
-      title : "",
       options : [],
       onChange : function(){},
       name : ""
@@ -15,29 +14,55 @@ var SelectDropdown = React.createClass({
     options: React.PropTypes.array,
     onChange : React.PropTypes.func
   },
+  getInitialState: function() {
+    return {title: this.props.options[0].name};
+  },
   render : function(){
     return (
-      <span className="select-box">
+      <span className="select-box" style={this._getStyles()}>
         <span className="title">{this._determineTitle()}</span>
-        <select name={this.props.name} ref="select" onChange={this._handleChange}>
+        <select name={this.props.name} ref="select" onChange={this._handleChange} value={this._determineSelectedOption()}>
             {this._buildOptions()}
         </select>
       </span>
     );
   },
-  _buildOptions : function(){
-    return this.props.options.map(function(option, index){
-      return (<option value={option.value}>{option.name}</option>);
-    });
+  _getStyles : function(){
+    return {
+      width : "200px"
+    };
   },
-  _determineTitle :  function(){
-    console.log(this.refs);
-    // return React.findDOMNode(this.refs.select).value
+  _determineTitle : function(){
+    var title = this.state.title;
+    this.props.options.map(function(option){
+      if(option.selected){
+        title = option.name;
+      }
+    });
+    return title;
+  },
+  _determineSelectedOption : function(){
+    var selctedOptionValue = -1;
+    this.props.options.map(function(option){
+      if(option.selected){
+        selctedOptionValue = option.value;
+      }
+    });
+    return selctedOptionValue;
+  },
+  _buildOptions : function(){
+    return this.props.options.map(function(option){
+      return (<option value={option.value} key={option.value}>{option.name}</option>);
+    });
   },
   _handleChange : function(event){
     this.props.onChange(event.target.value);
+    this._updateTitle();
+  },
+  _updateTitle : function(){
+    var select = this.refs.select.getDOMNode();
+    this.setState({title : select.options[select.selectedIndex].text});
   }
 });
 
 module.exports = SelectDropdown;
-
