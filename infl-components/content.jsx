55eff642f9ef51f46e17133ed8dda932faaaa999
Nick Faulkner <nick.faulkner@influitive.com>
2015-04-01 09:48:14 -0400

    updated content panel to handle additional purposes

--- infl-components/content.jsx
+++ infl-components/content.jsx
@@ -1,14 +1,36 @@
 var React = require('react');
 
 var Content = React.createClass({
+  getDefaultProps : function(){
+    return {
+      hasInnerPanel : true,
+      hasBackgroundColour : true
+    };
+  },
+  propTypes : {
+    hasInnerPanel: React.PropTypes.bool,
+    hasBackgroundColour: React.PropTypes.bool
+  },
   render: function () {
     return (
-      <div className="panel-content">
+      <div className={"panel-content " + this._doesContentHaveBackgroundColour()}>
+        {this._contentHasInnerPanel()}
+      </div>
+    );
+  },
+  _doesContentHaveBackgroundColour : function(){
+    return this.props.hasBackgroundColour ? "" : "no-colour";
+  },
+  _contentHasInnerPanel : function(){
+    if(this.props.hasInnerPanel){
+      return (
         <div className="panel-content-inner">
           {this.props.children}
         </div>
-      </div>
-    );
+      );
+    } else {
+      return (this.props.children);
+    }
   }
 });
 
--- infl-patternlab/source/js/infl-components.js
+++ infl-patternlab/source/js/infl-components.js
@@ -294,14 +294,36 @@ module.exports = ButtonDropdown;
 var React = require('react');
 
 var Content = React.createClass({displayName: "Content",
+  getDefaultProps : function(){
+    return {
+      hasInnerPanel : true,
+      hasBackgroundColour : true
+    };
+  },
+  propTypes : {
+    hasInnerPanel: React.PropTypes.bool,
+    hasBackgroundColour: React.PropTypes.bool
+  },
   render: function () {
     return (
-      React.createElement("div", {className: "panel-content"}, 
+      React.createElement("div", {className: "panel-content " + this._doesContentHaveBackgroundColour()}, 
+        this._contentHasInnerPanel()
+      )
+    );
+  },
+  _doesContentHaveBackgroundColour : function(){
+    return this.props.hasBackgroundColour ? "" : "no-colour";
+  },
+  _contentHasInnerPanel : function(){
+    if(this.props.hasInnerPanel){
+      return (
         React.createElement("div", {className: "panel-content-inner"}, 
           this.props.children
         )
-      )
-    );
+      );
+    } else {
+      return (this.props.children);
+    }
   }
 });
 
