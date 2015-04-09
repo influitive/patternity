4e9b658483887463f3024ec3672443d04f9b5dc7
Nick Faulkner <nick.faulkner@influitive.com>
2015-04-09 09:44:31 -0400

    refactored tests

--- infl-components/spec/sidebar_spec.js
+++ infl-components/spec/sidebar_spec.js
@@ -4,6 +4,7 @@ var es5Shim   = require('es5-shim'),
     TestUtils = require('react/addons').addons.TestUtils;
 
 var Sidebar = require('sidebar');
+var SidebarHeading = require('sidebar').heading;
 
 describe('Sidebar', function() {
   it('will be empty if it has no children', function () {
@@ -34,6 +35,43 @@ describe('Sidebar', function() {
     );
 
     expect(subject.refs.sidebar.getDOMNode().childNodes.length).to.equal(3);
+  });
+
+  describe('SidebarHeading', function() {
+    var heading, title;
+
+    beforeEach(function(){
+      title = "test title";
+      heading = {
+        headingComponent : "span",
+        title : title,
+        headingComponentParams : {
+          testParam : "testParam"
+        }
+      };
+    });
+
+    it('will have the correct title', function () {
+      var subject = TestUtils.renderIntoDocument(<SidebarHeading {...heading} />);
+
+      expect(subject.refs.heading.getDOMNode().firstChild.firstChild.firstChild.nodeValue).to.equal(title);
+    });
+
+    //Not sure how to test this case within React
+    xit('will NOT have the message if one is not passed', function () {
+      var subject = TestUtils.renderIntoDocument(<SidebarHeading {...heading} />);
+
+      // expect(subject.refs.heading.getDOMNode().lastChild.firstChild.nodeValue).to.equal(message);
+    });
+
+    it('will have the message if one is passed', function () {
+      var message = "test message";
+      heading.message = message;
+      var subject = TestUtils.renderIntoDocument(<SidebarHeading {...heading} />);
+
+      expect(subject.refs.heading.getDOMNode().lastChild.firstChild.nodeValue).to.equal(message);
+    });
+
   });
 });
 
