80e7113ade93b53d4c0c7c8f5f0036c0a9c7d3d7
Nick Faulkner <nick.faulkner@influitive.com>
2015-04-13 13:04:17 -0400

    updated label in put and form to handle multiple inputs for one label

--- infl-components/input_label.jsx
+++ infl-components/input_label.jsx
@@ -13,13 +13,16 @@ var InputLabel = React.createClass({
   },
   render : function(){
     return (
-      <span className={"pt-label " + this.props.layout}>
-        <label htmlFor={this._determineLabelFor()}>
+      <span className={"pt-label " + this.props.layout + " " + this._multiInput()}>
+        <label htmlFor={this._determineLabelFor()} ref="label">
           <span>{this.props.label + ":"}</span>
         </label>
         {this.props.children}
       </span>
     );
+  },
+  _multiInput : function(){
+    return this.props.children.length > 0 ? "multi-input" : "";
   },
   _determineLabelFor : function(){
     if(this.props.children.length > 0){
