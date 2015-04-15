0a877e44e4086c14ec6f1a11fcb55320f6e27f24
Dan Steinman <dansteinman@gmail.com>
2015-04-15 13:06:53 -0400

    upgrade.sh set DIR

--- script/upgrade.sh
+++ script/upgrade.sh
@@ -4,5 +4,5 @@ echo "UPGRADING PATTERNITY..."
 ulimit -S -n 2048
 git pull --rebase
 DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
-bundle install --path=$DIR
+bundle install --path=$DIR/..
 npm install
