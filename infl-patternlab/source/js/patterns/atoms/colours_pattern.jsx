3cd1c02d3cc27153e08b9d66b9c24f8818117a5a
Nick Faulkner <nick.faulkner@influitive.com>
2015-04-21 11:36:29 -0400

    fixed colours not having styles

--- infl-patternlab/source/js/patterns/atoms/colours_pattern.jsx
+++ infl-patternlab/source/js/patterns/atoms/colours_pattern.jsx
@@ -10,76 +10,76 @@ var ColourPattern = React.createClass({
         <Pattern title="colours">
           <p>The colours are set via sass variables and can be found in theme.scss.  The first two can change via white labeling, and are set by each company.</p>
 
-          <ul class="sg-colors">
+          <ul className="sg-colors">
             <li>
-              <span class="sg-swatch color-primary-background"></span>
-              <span class="sg-name">
+              <span className="sg-swatch color-primary-background"></span>
+              <span className="sg-name">
                 <strong>Bright Blue</strong>
               </span>
-              <span class="sg-label">#08a5c5</span>
+              <span className="sg-label">#08a5c5</span>
             </li>
             <li>
-              <span class="sg-swatch color-secondary-background"></span>
-              <span class="sg-name">
+              <span className="sg-swatch color-secondary-background"></span>
+              <span className="sg-name">
                 <strong>Dark Blue</strong>
               </span>
-              <span class="sg-label">#046f99</span>
+              <span className="sg-label">#046f99</span>
             </li>
             <li>
-              <span class="sg-swatch color-success-background"></span>
-              <span class="sg-name">
+              <span className="sg-swatch color-success-background"></span>
+              <span className="sg-name">
                 <strong>Green</strong>
               </span>
-              <span class="sg-label">#8BC540</span>
+              <span className="sg-label">#8BC540</span>
             </li>
             <li>
-              <span class="sg-swatch color-error-background"></span>
-              <span class="sg-name">
+              <span className="sg-swatch color-error-background"></span>
+              <span className="sg-name">
                 <strong>Red</strong>
               </span>
-              <span class="sg-label">#c54040</span>
+              <span className="sg-label">#c54040</span>
             </li>
             <li>
-              <span class="sg-swatch color-warning-background"></span>
-              <span class="sg-name">
+              <span className="sg-swatch color-warning-background"></span>
+              <span className="sg-name">
                 <strong>Yellow</strong>
               </span>
-              <span class="sg-label">#ffcc00</span>
+              <span className="sg-label">#ffcc00</span>
             </li>
             <li>
-              <span class="sg-swatch color-darker-grey-background"></span>
-              <span class="sg-name">
+              <span className="sg-swatch color-darker-grey-background"></span>
+              <span className="sg-name">
                 <strong>Darker Grey</strong>
               </span>
-              <span class="sg-label">#444444</span>
+              <span className="sg-label">#444444</span>
             </li>
             <li>
-              <span class="sg-swatch color-dark-grey-background"></span>
-              <span class="sg-name">
+              <span className="sg-swatch color-dark-grey-background"></span>
+              <span className="sg-name">
                 <strong>Dark Grey</strong>
               </span>
-              <span class="sg-label">#666666</span>
+              <span className="sg-label">#666666</span>
             </li>
             <li>
-              <span class="sg-swatch color-medium-grey-background"></span>
-              <span class="sg-name">
+              <span className="sg-swatch color-medium-grey-background"></span>
+              <span className="sg-name">
                 <strong>Medium Grey</strong>
               </span>
-              <span class="sg-label">#666666</span>
+              <span className="sg-label">#666666</span>
             </li>
             <li>
-              <span class="sg-swatch color-light-grey-background"></span>
-              <span class="sg-name">
+              <span className="sg-swatch color-light-grey-background"></span>
+              <span className="sg-name">
                 <strong>Light Grey</strong>
               </span>
-              <span class="sg-label">#d0d2d3</span>
+              <span className="sg-label">#d0d2d3</span>
             </li>
             <li>
-              <span class="sg-swatch color-lighter-grey-background"></span>
-              <span class="sg-name">
+              <span className="sg-swatch color-lighter-grey-background"></span>
+              <span className="sg-name">
                 <strong>Lighter Grey</strong>
               </span>
-              <span class="sg-label">#eeeeee</span>
+              <span className="sg-label">#eeeeee</span>
             </li>
           </ul>
 
