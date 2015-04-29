020a80530f836f640dbc6fd30899b86beb0835ea
Dan Steinman <dansteinman@gmail.com>
2015-04-29 10:03:27 -0400

    gulp task to generate influicons from SVGs

---
+++ influicons/gulpfile.js
@@ -0,0 +1,37 @@
+var gulp = require('gulp');
+var iconfont = require('gulp-iconfont');
+var consolidate = require('gulp-consolidate');
+ 
+gulp.task('influicons', function(){
+  gulp.src(['SVG/*.svg'])
+    .pipe(iconfont({
+      fontName: 'influicons',
+      fixedWidth: true,
+      centerHorizontally : true,
+      normalize: true
+    }))
+
+    .on('codepoints', function(codepoints, options) {
+
+      gulp.src('templates/influicons.css')
+        .pipe(consolidate('lodash', {
+          glyphs: codepoints,
+          fontName: 'influicons',
+          fontPath: 'fonts/',
+          className: 'ic'
+        }))
+        .pipe(gulp.dest('fonts/'));
+
+      gulp.src('templates/influicons.html')
+        .pipe(consolidate('lodash', {
+          glyphs: codepoints,
+          fontName: 'influicons',
+          fontPath: 'fonts/',
+          className: 'ic'
+        }))
+        .pipe(gulp.dest('fonts/'));
+
+    })
+    .pipe(gulp.dest('fonts/'));
+});
+
