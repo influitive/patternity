3a526db43767385b02c75326444ac492f7ec0446
Dan Steinman <dansteinman@gmail.com>
2015-05-13 09:17:44 -0400

    new Popover docs, and first pass of making an example code/demo includer tool

--- infl-patternlab/source/js/patterns/molecules/popover_pattern.jsx
+++ infl-patternlab/source/js/patterns/molecules/popover_pattern.jsx
@@ -3,44 +3,19 @@ var Pattern   = require('../../patternlab-components/pattern.jsx');
 var Code      = require('../../patternlab-components/code.jsx');
 var Require   = require('../../patternlab-components/require.jsx');
 
-var Popover       = require("../../../../infl-components/popover.jsx");
+var Example1 = require('./popover_example1.jsx');
 
 var PopoverPattern = React.createClass({
   render : function(){
     return (
       <div className="popover-pattern">
         <Pattern title="popover">
-          <p>The Popover component is a floating component that is used to hide and show content.</p>
-          <p>Use an anchor tag with a <b>data-popover</b> attribute to reference the popover to open and bind the <b>Popover.clickEvent</b> function to the onClick event.</p>
+          <p>The Popover component is a floating menu used to expose more options.</p>
 
           <Pattern.Detail title="Popover">
-            <Pattern.Show>
-              <a href="javascript://" data-popover="MyPopover1" onClick={ Popover.clickEvent.bind(this) }>Open "MyPopover1"</a>
-
-              <Popover ref="MyPopover1">
-                <ul>
-                  <li><a className="ic ic-pencil"href="javascript://">Edit</a></li>
-                  <li><a className="ic ic-lock"href="javascript://">Lock</a></li>
-                  <li><a className="ic ic-arrow-left"href="javascript://">Move</a></li>
-                  <li><a className="ic ic-trash" href="javascript://">Delete</a></li>
-                </ul>
-              </Popover>
-            </Pattern.Show>
-
+            <Example1.Demo/>
             <Code>
-              <Code.JSX>
-              &lt;a href="javascript://" data-popover="MyPopover1" onClick=&#123; Popover.clickEvent.bind(this) &#125;&gt;Open "MyPopover1"&lt;/a&gt;
-
-              &lt;Popover ref="MyPopover1"&gt;
-              &lt;ul&gt;
-              &lt;li&gt;&lt;a className="ic ic-pencil"href="javascript://"&gt;Edit&lt;/a&gt;&lt;/li&gt;
-              &lt;li&gt;&lt;a className="ic ic-lock"href="javascript://"&gt;Lock&lt;/a&gt;&lt;/li&gt;
-              &lt;li&gt;&lt;a className="ic ic-arrow-left"href="javascript://"&gt;Move&lt;/a&gt;&lt;/li&gt;
-              &lt;li&gt;&lt;a className="ic ic-trash" href="javascript://"&gt;Delete&lt;/a&gt;&lt;/li&gt;
-              &lt;/ul&gt;
-              &lt;/Popover&gt;
-              </Code.JSX>
-
+              <Example1.Code/>
               <Code.Props patternProps={this._getProps()} />
             </Code>
           </Pattern.Detail>
