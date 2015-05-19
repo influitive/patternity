8b9aba7324e6beb1e86307825d5159c0420b6175
Nick Faulkner <nick.faulkner@influitive.com>
2015-05-19 15:38:44 -0400

    added card container to help with styling of cards

--- infl-components/spec/card_spec.js
+++ infl-components/spec/card_spec.js
@@ -5,31 +5,61 @@ var es5Shim   = require('es5-shim'),
 
 var Card = require('cards/card.jsx');
 
-describe('Card', function() {
-  var subject, cardElement;
+describe('Card Component', function() {
+  describe('Card', function () {
+    var subject, cardElement;
 
-  function renderCardComponent(cardComponent) {
-    subject = TestUtils.renderIntoDocument(cardComponent);
-    populateTestRefs();
-  }
+    function renderCardComponent(cardComponent) {
+      subject = TestUtils.renderIntoDocument(cardComponent);
+      populateTestRefs();
+    }
 
-  function populateTestRefs(){
-    cardElement = subject.refs.card.getDOMNode();
-  }
+    function populateTestRefs(){
+      cardElement = subject.refs.card.getDOMNode();
+    }
 
-  it('will render the card component', function () {
-    renderCardComponent(<Card></Card>);
-    expect(cardElement.className).to.contain("pt-card");
-    expect(cardElement.tagName).to.equal("DIV");
+    it('will render the card component', function () {
+      renderCardComponent(<Card></Card>);
+      expect(cardElement.className).to.contain("pt-card");
+      expect(cardElement.tagName).to.equal("DIV");
+    });
+
+    it('will render children passed to it', function () {
+      renderCardComponent(
+        <Card>
+          <p>test</p>
+        </Card>
+      );
+      expect(cardElement.children.length).to.equal(1);
+    });
   });
 
-  it('will render children passed to it', function () {
-    renderCardComponent(
-      <Card>
-        <p>test</p>
-      </Card>
-    );
-    expect(cardElement.children.length).to.equal(1);
+  describe('Card Container', function () {
+    var subject, cardContainerElement;
+
+    function renderCardContainerComponent(cardContainerComponent) {
+      subject = TestUtils.renderIntoDocument(cardContainerComponent);
+      populateTestRefs();
+    }
+
+    function populateTestRefs(){
+      cardContainerElement = subject.refs.container.getDOMNode();
+    }
+
+    it('will render the card component', function () {
+      renderCardContainerComponent(<Card.Container></Card.Container>);
+      expect(cardContainerElement.className).to.contain("pt-card-container");
+      expect(cardContainerElement.tagName).to.equal("DIV");
+    });
+
+    it('will render children passed to it', function () {
+      renderCardContainerComponent(
+        <Card.Container>
+          <Card></Card>
+        </Card.Container>
+      );
+      expect(cardContainerElement.children.length).to.equal(1);
+    });
   });
 });
 
