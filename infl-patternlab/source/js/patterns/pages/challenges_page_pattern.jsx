b7876967a3607a692aab4faf4016c1e80621c2a5
Nick Faulkner <nick.faulkner@influitive.com>
2015-05-26 13:11:53 -0400

    updated challenge card demo

--- infl-patternlab/source/js/patterns/pages/challenges_page_pattern.jsx
+++ infl-patternlab/source/js/patterns/pages/challenges_page_pattern.jsx
@@ -182,7 +182,7 @@ var ChallengesPagePattern = React.createClass({
     return (
       <div className="challenges-page-pattern page-pattern">
         <Pattern title="challenges page demo">
-          <Tabs showAllTabs={true}>
+          <Tabs showAllTabs={false}>
             <Tabs.Tab title="Available" id="available-tab">
               <Card.Container>
                 {this._buildCards(this.state.available.challenges)}
