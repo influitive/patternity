d2c2911043fba28576b0fd70b54e1795120fb260
Nick Faulkner <nick.faulkner@influitive.com>
2015-06-22 15:26:45 -0400

    Challenge Cards: limited headline to 3 lines

--- infl-components/cards/challenge_card.jsx
+++ infl-components/cards/challenge_card.jsx
@@ -110,14 +110,55 @@ ChallengeCard.Details = React.createClass({
     };
   },
 
+  componentDidMount : function(){
+    this._adjustHeadline();
+  },
+
+  componentDidUpdate : function(){
+    this._adjustHeadline();
+  },
+
   render : function(){
     return (
       <div className="pt-challenge-details">
-        <h4 className="headline" onClick={this.props.onHeadlineClick}>{this.props.headline}</h4>
+        <h4 className="headline" ref="headline" onClick={this.props.onHeadlineClick}>{this.props.headline}</h4>
         <ChallengeTypeCount type={this.props.type} onClick={this.props.onFilterByType} participantCount={this.props.participantCount} />
         <p ref="description" className="description" dangerouslySetInnerHTML={this._sanitizeDescription()}></p>
       </div>
     );
+  },
+
+  _adjustHeadline : function(){
+    var headline = React.findDOMNode(this.refs.headline);
+    var headlineMaxHeight = parseInt($(headline).css("max-height"));
+
+    if(headline.clientHeight === headlineMaxHeight){
+      this._ellipsisHeadlineText(headline, headlineMaxHeight);
+    }
+  },
+
+  _ellipsisHeadlineText : function(headline, headlineMaxHeight){
+    this._convertWordsToElements(headline);
+    var lastVisibleWordElement = this._findLastVisibleWord(headline, headlineMaxHeight);
+    $(lastVisibleWordElement).addClass("last-visible-word");
+  },
+
+  _convertWordsToElements : function(headline) {
+    $(headline).html('<span>' + $(headline).html().replace(/ /g,'</span> <span>') + '</span>');
+  },
+
+  _findLastVisibleWord : function(headline, headlineMaxHeight){
+    var words = $(headline).find("span");
+    var lastVisibleWordElement = null;
+
+    for(var i = 0; i < words.length; i++){
+      if($(words[i]).position().top >= headlineMaxHeight){
+        lastVisibleWordElement = words[i-1];
+        break;
+      }
+    }
+
+    return lastVisibleWordElement;
   },
 
   _sanitizeDescription : function(){
