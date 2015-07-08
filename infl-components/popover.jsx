cbd3c5d5e9f9c60813bcacfdb7057012ba010e95
Dan Steinman <daniel@influitive.com>
2015-07-08 16:13:08 -0400

    Popover mobile fixes, event bubbling changes

--- infl-components/popover.jsx
+++ infl-components/popover.jsx
@@ -3,39 +3,64 @@ window.React = React;
 
 var $ = require('jquery');
 
+function cancelEvent(e) {
+  e.stopImmediatePropagation();
+  e.stopPropagation();
+  e.preventDefault();
+  e.cancelBubble = true;
+  return false;
+}
+
 var PopoverFloater = React.createClass({
 
   propTypes : {
     children: React.PropTypes.object,
     targetElement: React.PropTypes.object,
     onClick: React.PropTypes.func,
-    onBlur: React.PropTypes.func
+    onHide: React.PropTypes.func,
+    autoclose: React.PropTypes.bool
   },
 
   getInitialState: function() {
     return {
-      top: 0,
-      left: 0,
-      recalculated: false
+      edgeRestricted : false
     };
   },
 
   componentDidUpdate : function() {
-    if (!this.state.recalculated) {
-      this._recalculatePosition();
-    }
+    this.resetPosition();
   },
 
   componentDidMount : function() {
-    this._recalculatePosition();
+    this.resetPosition();
+    this._bindWindowEvents();
+  },
+
+  componentWillUnmount : function() {
+    this._unbindWindowEvents();
+  },
+
+  _bindWindowEvents: function() {
+    if (this.props.onHide) {
+      var me = this;
+      this._hidePopoverEvent = function(e) {
+        me.props.onHide(e); // communicates up to the parent Popover to hide
+      };
+      $(document).on('click', this._hidePopoverEvent);
+    }
+  },
+
+  _unbindWindowEvents: function() {
+    if (this.props.onHide) {
+      $(document).off('click', this._hidePopoverEvent);
+    }
   },
 
   render: function() {
     // this line makes it so the contents of the popover are not rendered until they are needed, and not destroyed when hidden
     var children = this.props.children;
-
     return (
-      <div ref="popover" className={this._classes()} style={this._getStyle()} onClick={this._menuItemClicked}>
+      <div ref="popover" className={this._classes()} onClick={this._menuItemClicked}>
         <div className="arrow-top"/>
         <div className="arrow-top-inner"/>
         { children }
@@ -44,29 +69,29 @@ var PopoverFloater = React.createClass({
   },
 
   _menuItemClicked: function(e) {
-    if (this.props.onClick) { this.props.onClick(e); }
-  },
-
-  _getStyle: function() {
-    return {
-      top: this.state.top + 'px',
-      left: this.state.left + 'px'
-    };
+    // if autoclose is turned off, clicking inside the popover cancels the event bubbling before reaching the document which stops the hidePopover event
+    if (!this.props.autoclose) {
+      var realEvent = e.nativeEvent? e.nativeEvent : e;
+      return cancelEvent(realEvent);
+    }
   },
 
   _classes: function() {
-    return 'pt-popover is-visible';
+    var c = 'pt-popover is-visible';
+    return c;
   },
 
   resetPosition: function() {
-    var targetElement = this.props.targetElement;
+    var targetElementNode = React.findDOMNode(this.props.targetElement);
+    var targetElement = $(targetElementNode);
     var popoverNode = React.findDOMNode(this.refs.popover);
 
     var popover = $(popoverNode);
-    var tW = $(targetElement).width();
-    var tH = $(targetElement).height();
-    var tOT = $(targetElement).position().top;
-    var tOL = $(targetElement).position().left;
+    var tW = targetElement.width();
+    var tH = targetElement.height();
+    var tp = targetElement.position();
+    var tOT = tp.top;
+    var tOL = tp.left;
     var pW = popover.width();
     //var pH = popover.height();
 
@@ -78,62 +103,33 @@ var PopoverFloater = React.createClass({
     // restrict right edge to the width of the screen
     if (left + pW + 10 > window.innerWidth) {
       left = window.innerWidth - pW - 10;
+      $(popoverNode).addClass('edge-restricted');
       // todo: in this scenario the arrow pointing to the target element would need to be manually positioned instead of being in the middle
+    }
+
+    // if the parent element is absolutely positioned need to account for that
+    var isAbsolute = popoverNode.parentNode.className.indexOf('is-absolute')>-1;
+
+    if (!isAbsolute && left < 0) {
+      left = 5;
+      $(popoverNode).addClass('edge-restricted');
     }
 
     popoverNode.style.top = top+'px';
     popoverNode.style.left = left+'px';
 
     var offset = popover.offset();
-    // if the parent element is absolutely positioned need to account for that
-    var isAbsolute = popoverNode.parentNode.className.indexOf('is-absolute')>-1;
+
     if (isAbsolute && offset.left + pW + 10 > window.innerWidth) {
       var pO = $(popoverNode.parentNode).offset();
       var absLeft = window.innerWidth - pW - 10;
       var relLeft = absLeft - pO.left;
       popoverNode.style.left = relLeft+'px';
-    }
-  },
-
-  _recalculatePosition: function() {
-    var targetElement = React.findDOMNode(this.props.targetElement);
-    var popoverNode = React.findDOMNode(this);
-
-    var popover = $(popoverNode);
-    var tW = $(targetElement).width();
-    var tH = $(targetElement).height();
-    var tOT = $(targetElement).position().top;
-    var tOL = $(targetElement).position().left;
-    var pW = popover.width();
-    //var pH = popover.height();
-
-    // position the popover centered below the target element
-    var top = tOT + tH + 10;
-    if (top<0) top = 0;
-    var left = tOL + (tW - pW)/2;
-
-    // restrict right edge to the width of the screen
-    if (left + pW + 10 > window.innerWidth) {
-      left = window.innerWidth - pW - 10;
-      // todo: in this scenario the arrow pointing to the target element would need to be manually positioned instead of being in the middle
+      $(popoverNode).addClass('edge-restricted');
     }
 
-    var offset = popover.offset();
-    // if the parent element is absolutely positioned need to account for that
-    var isAbsolute = popoverNode.parentNode.className.indexOf('is-absolute')>-1;
-    if (isAbsolute && offset.left + pW + 10 > window.innerWidth) {
-      var pO = $(popoverNode.parentNode).offset();
-      var absLeft = window.innerWidth - pW - 10;
-      var relLeft = absLeft - pO.left;
-      left = relLeft;
-    }
-
-    this.setState({
-      top: top,
-      left: left,
-      recalculated: true
-    });
   }
+
 });
 
 var Popover = React.createClass({
@@ -162,36 +158,17 @@ var Popover = React.createClass({
 
   _renderPopover: function() {
     if (this.state.isVisible) {
-      if (this.props.autoclose) { this._bindWindowEvents(); }
-
       var contents = this.props.children[1];
       return (
-        <PopoverFloater ref="popover" targetElement={this.refs.link} onClick={this._popoverClicked}>
+        <PopoverFloater ref="popover" autoclose={this.props.autoclose} targetElement={this.refs.link} onHide={this._hidePopover}>
           { contents }
         </PopoverFloater>
       );
-    } else {
-      if (this.props.autoclose) { this._unbindWindowEvents(); }
     }
   },
 
   resetPosition: function() {
     this.refs.popover.resetPosition();
-  },
-
-  _bindWindowEvents: function() {
-    $(document).on('click', this._hidePopover.bind(this));
-    // $(document).on('touchstart', this._hidePopover.bind(this));
-  },
-
-  _unbindWindowEvents: function() {
-    $(document).off('click', this._hidePopover.bind(this));
-  },
-
-  _popoverClicked: function(e) {
-    if (this.props.autoclose) {
-      this._hidePopover();
-    }
   },
 
   _onOpen: function() {
@@ -217,11 +194,11 @@ var Popover = React.createClass({
   },
 
   _hidePopover: function() {
+    this._onClose();
+
     this.setState({
       isVisible: false
     });
-
-    this._onClose();
   }
 });
 
