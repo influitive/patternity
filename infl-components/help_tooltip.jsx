77221ebc338562d6283be39b5a8e691ea628b9b8
Nick Faulkner <nick.faulkner@influitive.com>
2015-07-09 10:46:13 -0400

    Tooltip: refactored help tooltip to make a Toolip component

--- infl-components/help_tooltip.jsx
+++ infl-components/help_tooltip.jsx
@@ -1,85 +1,25 @@
 var React = require('react');
 
+var Icon = require('./icon.jsx');
+var Tooltip = require('./tool_tip.jsx');
+
 var HelpTooltip = React.createClass({
+  propTypes : {
+    title: React.PropTypes.string
+  },
+
   getDefaultProps: function(){
     return {
       title : ""
     };
   },
-  getInitialState: function() {
-    return {
-      showTooltip: false,
-      showClose: false,
-      wasClicked: false
-    };
-  },
-  propTypes : {
-    title: React.PropTypes.string
-  },
+
   render : function(){
     return (
-      <span className="help-tooltip" ref="helpTooltip">
-        <div className={"tooltip-content " + this._showTooltip()} ref="tip">
-          <span className={"close ic ic-times " + this._showClose()} onClick={this._clickCloseTooltip} ref="close"></span>
-          <h3 ref="title">{this.props.title}</h3>
-          <div className="tooltip-details" ref="details">
-            {this.props.children}
-          </div>
-        </div>
-        <span className="help ic ic-question-circle-o"
-          onClick={this._clickTooltip}
-          onTouchStart={this._clickShowTooltip}
-          onMouseEnter={this._hoverShowTooltip}
-          onMouseLeave={this._hoverHideTooltip}
-          ref="help">
-        </span>
-      </span>
+      <Tooltip title={this.props.title} element={<Icon icon="question-circle-o" />} >
+        {this.props.children}
+      </Tooltip>
     );
-  },
-  _showTooltip : function(){
-    return this.state.showTooltip ? "" : "hide";
-  },
-  _showClose: function(){
-    return this.state.showClose ? "" : "hide";
-  },
-  _hoverShowTooltip: function() {
-    this._hoverToggleTooltip(true);
-  },
-  _hoverHideTooltip: function() {
-    this._hoverToggleTooltip(false);
-  },
-  _hoverToggleTooltip : function(shouldShow){
-    if(!this.state.wasClicked){
-      this._updateState({
-        showTooltip: shouldShow,
-        showClose: false,
-        wasClicked: false
-      });
-    }
-  },
-  _clickTooltip : function() {
-    if (this.state.showTooltip && this.state.wasClicked) {
-      this._clickCloseTooltip();
-    } else {
-      this._clickShowTooltip();
-    }
-  },
-  _clickShowTooltip : function() {
-    this._updateState({
-      showTooltip: true,
-      showClose: true,
-      wasClicked: true
-    });
-  },
-  _clickCloseTooltip : function(){
-    this._updateState({
-      showTooltip: false,
-      showClose: true,
-      wasClicked: false
-    });
-  },
-  _updateState : function(newState){
-    this.setState(newState);
   }
 });
 
