7bf7b7f82352af917a97c75f1d3ef14edef44a90
Shane Keulen <shane.keulen@gmail.com>
2015-08-19 11:44:30 -0400

    style

--- infl-components/radio_button.jsx
+++ infl-components/radio_button.jsx
@@ -4,37 +4,40 @@ var classNames = require('classnames');
 var RadioButton = React.createClass({
   getDefaultProps: function() {
     return {
-      id: "",
-      enabled: true,
-      isChecked : false,
-      onChange: function(){},
-      radioName : "",
-      radioLabel : "",
-      value : ""
+      id:         '',
+      enabled:    true,
+      isChecked:  false,
+      onChange:   function() {},
+      radioName:  '',
+      radioLabel: '',
+      value:      ''
     };
   },
-  propTypes : {
-    id: React.PropTypes.string,
-    enabled: React.PropTypes.bool,
-    isChecked: React.PropTypes.bool,
-    onChange : React.PropTypes.func,
-    radioName : React.PropTypes.string,
-    radioLabel : React.PropTypes.string,
-    value : React.PropTypes.string
+
+  propTypes: {
+    id:         React.PropTypes.string,
+    enabled:    React.PropTypes.bool,
+    isChecked:  React.PropTypes.bool,
+    onChange:   React.PropTypes.func,
+    radioName:  React.PropTypes.string,
+    radioLabel: React.PropTypes.string,
+    value:      React.PropTypes.string
   },
-  getInitialState : function(){
+
+  getInitialState: function() {
     return {
       isChecked: this.props.isChecked
     };
   },
-  componentWillReceiveProps: function (newProps) {
+
+  componentWillReceiveProps: function(newProps) {
     this.setState({
       isChecked: newProps.isChecked
     });
   },
-  render : function(){
-    return (
-      <span id={this.props.id} className={this._radioCSSClasses()} onClick={this._clickRadioButton} ref="radioButton">
+
+  render: function() {
+    return <span id={this.props.id} className={this._radioCSSClasses()} onClick={this._clickRadioButton} ref="radioButton">
         <input disabled={!this.props.enabled}
             type="radio"
             ref="nativeRadioButton"
@@ -45,42 +48,44 @@ var RadioButton = React.createClass({
             onChange={this._handleChange} />
         <span className="stylized-radio-button" ref="stylizedRadioButton"></span>
         <span className="pt-radio-label" ref="label">{this.props.radioLabel}</span>
-      </span>
-    );
+      </span>;
   },
-  _radioCSSClasses : function(){
+
+  _radioCSSClasses: function() {
     return classNames({
       'pt-radio-button': true,
-      'disabled': !this.props.enabled
+      'disabled':        !this.props.enabled
     });
   },
-  _clickRadioButton : function(){
-    if(this.props.enabled){
+
+  _clickRadioButton: function() {
+    if(this.props.enabled) {
       React.findDOMNode(this.refs.nativeRadioButton).click();
     }
   },
-  _handleChange : function(event){
+
+  _handleChange: function(event) {
     this.props.onChange(event);
   }
 });
 
 RadioButton.Group = React.createClass({
-    getDefaultProps: function() {
+  getDefaultProps: function() {
     return {
-      id: "",
-      layout : "inline"
+      id:     '',
+      layout: 'inline'
     };
   },
-  propTypes : {
-    id: React.PropTypes.string,
-    layout : React.PropTypes.string
+
+  propTypes: {
+    id:     React.PropTypes.string,
+    layout: React.PropTypes.string
   },
-  render : function(){
-    return (
-      <span className={"pt-radio-button-group " + this.props.layout} id={this.props.id} ref="radioButtonGroup">
+
+  render: function() {
+    return <span className={'pt-radio-button-group ' + this.props.layout} id={this.props.id} ref='radioButtonGroup'>
         {this.props.children}
-      </span>
-    );
+      </span>;
   }
 });
 
