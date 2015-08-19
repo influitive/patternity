82cbd3a9ce01e63b6804ce90558ed9c48592c2a6
Shane Keulen <shane.keulen@gmail.com>
2015-08-19 10:54:32 -0400

    code style normalize

--- infl-components/text_input.jsx
+++ infl-components/text_input.jsx
@@ -4,63 +4,66 @@ var classNames = require('classnames');
 var TextInput = React.createClass({
   getDefaultProps: function() {
     return {
-      type: "text",
-      placeholder : "",
-      name: "",
-      id : "",
-      pattern : "",
-      message : "",
-      value : "",
-      required : false,
-      error : false,
-      valid : false,
-      readOnly : false,
-      disabled : false,
-      onChange : function(){},
-      clearable : false,
-      onCleared : function(){},
-      autofocus : false,
-      key : ""
+      type:        'text',
+      placeholder: '',
+      name:        '',
+      id:          '',
+      pattern:     '',
+      message:     '',
+      value:       '',
+      required:    false,
+      error:       false,
+      valid:       false,
+      readOnly:    false,
+      disabled:    false,
+      onChange:    function() {},
+      clearable:   false,
+      onCleared:   function() {},
+      autofocus:   false,
+      key:         ''
     };
   },
-  propTypes : {
-    type: React.PropTypes.oneOf(['text', 'password', 'url', 'email', 'search', 'number', '']),
-    placeholder : React.PropTypes.string,
-    name: React.PropTypes.string,
-    id : React.PropTypes.string,
-    pattern : React.PropTypes.string,
-    message : React.PropTypes.oneOfType([
+
+  propTypes: {
+    type:        React.PropTypes.oneOf(['text', 'password', 'url', 'email', 'search', 'number', '']),
+    placeholder: React.PropTypes.string,
+    name:        React.PropTypes.string,
+    id:          React.PropTypes.string,
+    pattern:     React.PropTypes.string,
+    message:     React.PropTypes.oneOfType([
       React.PropTypes.string,
       React.PropTypes.array
     ]),
-    value : React.PropTypes.oneOfType([
+    value: React.PropTypes.oneOfType([
       React.PropTypes.string,
       React.PropTypes.number
     ]),
-    required : React.PropTypes.bool,
-    error : React.PropTypes.bool,
-    valid : React.PropTypes.bool,
-    readOnly : React.PropTypes.bool,
-    disabled : React.PropTypes.bool,
-    onChange : React.PropTypes.func,
-    clearInput : React.PropTypes.bool,
-    onInputCleared : React.PropTypes.func,
-    autofocus : React.PropTypes.bool,
-    key : React.PropTypes.string
+    required:       React.PropTypes.bool,
+    error:          React.PropTypes.bool,
+    valid:          React.PropTypes.bool,
+    readOnly:       React.PropTypes.bool,
+    disabled:       React.PropTypes.bool,
+    onChange:       React.PropTypes.func,
+    clearInput:     React.PropTypes.bool,
+    onInputCleared: React.PropTypes.func,
+    autofocus:      React.PropTypes.bool,
+    key:            React.PropTypes.string
   },
+
   getInitialState: function() {
     return {value: this.props.value};
   },
-  componentWillReceiveProps: function(newProps){
-    this.setState({
-      value : newProps.value
-    });
+
+  componentWillReceiveProps: function(newProps) {
+    this.setState({ value: newProps.value });
     this._setInputFocus(newProps.autofocus);
   },
-  componentDidMount: function(){
+
+  componentDidMount: function() {
     this._setInputFocus(this.props.autofocus);
   },
-  render : function(){
+
+  render: function() {
     return (
       <span className={this._determineInputStyling()}>
         {this._determineInputIcon()}
@@ -72,54 +75,61 @@ var TextInput = React.createClass({
       </span>
     );
   },
-  _setInputFocus : function(autofocus){
-    if(autofocus){
+
+  _setInputFocus: function(autofocus) {
+    if (autofocus) {
       React.findDOMNode(this.refs.input).focus();
     }
   },
-  _determineInputStyling : function(){
+
+  _determineInputStyling: function() {
     return classNames({
-      'is-required': this.props.required,
-      'is-error': this.props.error,
-      'is-valid': this.props.valid,
+      'is-required':  this.props.required,
+      'is-error':     this.props.error,
+      'is-valid':     this.props.valid,
       'search-input': this.props.type === 'search',
-      'pt-input': true,
-      'is-disabled' : this.props.disabled,
-      'is-clearable' : this.props.clearable
+      'pt-input':     true,
+      'is-disabled':  this.props.disabled,
+      'is-clearable': this.props.clearable
     });
   },
-  _determineInputIcon : function(){
-    if(this.props.type === 'search'){
+
+  _determineInputIcon: function() {
+    if (this.props.type === 'search') {
       return (<span className="search-input ic ic-search"></span>);
-    } else if(this.props.required) {
+    } else if (this.props.required) {
       return (<span className="required-input ic ic-asterisk"></span>);
     }
   },
-  _handleChange: function(event){
-    this.setState({value : event.target.value});
+
+  _handleChange: function(event) {
+    this.setState({value: event.target.value});
     this.props.onChange(event);
   },
-  _isClearable : function(){
-    if(!this.props.clearable){
-      return "";
+
+  _isClearable: function() {
+    if(!this.props.clearable) {
+      return '';
     }
 
     return (
       <span className="clear-input ic ic-close" onClick={this._clearInputValue}></span>
     );
   },
-  _clearInputValue : function(){
+
+  _clearInputValue: function() {
     this.setState({
-      value : ""
+      value: ''
     }, this.props.onCleared());
   },
-  _buildMessage: function(){
-    if (typeof this.props.message === "string") {
-      return (<span className="input-message">{this.props.message}</span>);
+
+  _buildMessage: function() {
+    if (typeof this.props.message === 'string') {
+      return (<span className='input-message'>{this.props.message}</span>);
     }
     else {
-      return this.props.message.map(function(message, i){
-        return (<span key={i} className="input-message">{message}</span>);
+      return this.props.message.map(function(message, i) {
+        return (<span key={i} className='input-message'>{message}</span>);
       });
     }
   }
