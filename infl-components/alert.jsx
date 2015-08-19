a4e5d5cd5885dcb517a3a4f967388c67f3352c41
Shane Keulen <shane.keulen@gmail.com>
2015-08-19 13:24:33 -0400

    bump eslint version
    style changes

--- infl-components/alert.jsx
+++ infl-components/alert.jsx
@@ -7,35 +7,33 @@ var Alert = React.createClass({
   mixins: [AlertMixin],
   getDefaultProps: function() {
     return {
-      type: "",
-      showIcon: false,
-      closeable : false,
-      showAlert : true,
-      onClose : function(){},
-      hideIn  : 0
+      type:      '',
+      showIcon:  false,
+      closeable: false,
+      showAlert: true,
+      onClose:   function() {},
+      hideIn:    0
     };
   },
-  propTypes : {
-    title: React.PropTypes.string,
-    type: React.PropTypes.oneOf(['success', 'error', 'info', 'warning', '']),
-    closeable : React.PropTypes.bool,
-    showAlert : React.PropTypes.bool,
-    onClose: React.PropTypes.func,
-    hideIn: React.PropTypes.number
+  propTypes: {
+    title:     React.PropTypes.string,
+    type:      React.PropTypes.oneOf(['success', 'error', 'info', 'warning', '']),
+    closeable: React.PropTypes.bool,
+    showAlert: React.PropTypes.bool,
+    onClose:   React.PropTypes.func,
+    hideIn:    React.PropTypes.number
   },
-  render : function(){
-    return (
-      <div className={"alert-msg " + this.props.type + " " + this._hasIconClass() + " " + this._showAlert()} ref="alert">
-        {this._closeable()}
-        <h4 className="alert-title" ref="title">
-          {this._icon()}
-          <span>{this.props.title}</span>
-        </h4>
-        <div className="alert-body" ref="body">
-          {this.props.children}
-        </div>
+  render: function() {
+    return <div className={'alert-msg ' + this.props.type + ' ' + this._hasIconClass() + ' ' + this._showAlert()} ref='alert'>
+      {this._closeable()}
+      <h4 className='alert-title' ref='title'>
+        {this._icon()}
+        <span>{this.props.title}</span>
+      </h4>
+      <div className='alert-body' ref='body'>
+        {this.props.children}
       </div>
-    );
+    </div>;
   }
 });
 
--- infl-components/button.jsx
+++ infl-components/button.jsx
@@ -4,31 +4,28 @@ var buttonTypes = ['primary', 'secondary', 'important', 'success', 'danger', 'te
 
 var Button = React.createClass({
 
-  propTypes : {
-    icon: React.PropTypes.string,
+  propTypes: {
+    icon:      React.PropTypes.string,
     className: React.PropTypes.string,
-    type: React.PropTypes.string,
-    onClick: React.PropTypes.func,
-    href: React.PropTypes.string,
-    disabled: React.PropTypes.bool,
-    inverse: React.PropTypes.bool
+    type:      React.PropTypes.string,
+    onClick:   React.PropTypes.func,
+    href:      React.PropTypes.string,
+    disabled:  React.PropTypes.bool,
+    inverse:   React.PropTypes.bool
   },
 
-  render : function() {
-    return (
-      <button disabled={ this.props.disabled } className={ this._getClasses() } onClick={ this._onClick }>
-        { this.props.children }
-      </button>
-    );
+  render: function() {
+    return <button disabled={ this.props.disabled } className={ this._getClasses() } onClick={ this._onClick }>
+      { this.props.children }
+    </button>;
   },
 
-  _getClasses : function() {
+  _getClasses: function() {
     var classes = 'button';
 
     if (this.props.disabled) {
       classes += ' disabled';
-    }
-    else if (this.props.type && buttonTypes.indexOf(this.props.type)>-1) {
+    } else if (this.props.type && buttonTypes.indexOf(this.props.type)>-1) {
       classes += ' ' + this.props.type;
 
       if ((this.props.type==='secondary' || this.props.type==='text') && this.props.inverse) {
@@ -44,14 +41,13 @@ var Button = React.createClass({
     return classes;
   },
 
-
-  _onClick : function(e) {
+  _onClick: function(e) {
     if (this.props.onClick) {
-        var ret = this.props.onClick(e);
-        if (ret === false) return;
+      var ret = this.props.onClick(e);
+      if (ret === false) return;
     }
     if (this.props.href) {
-        document.location.href = this.props.href;
+      document.location.href = this.props.href;
     }
   }
 
