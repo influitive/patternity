bccec273b1c6fa6171b8caf51370b936d0f09b6a
Shane Keulen <shane.keulen@gmail.com>
2015-08-19 12:56:39 -0400

    style

--- infl-components/button_dropdown.jsx
+++ infl-components/button_dropdown.jsx
@@ -6,49 +6,64 @@ var ButtonDropdown = React.createClass({
       isDropdownOpen: false
     };
   },
+
   getDefaultProps: function() {
     return {
-      title: "",
-      type : "",
-      options : [],
-      children : [],
+      title:    '',
+      type:     '',
+      options:  [],
+      children: []
     };
   },
-  propTypes : {
+
+  propTypes: {
     title: React.PropTypes.string,
-    type: React.PropTypes.oneOf(['success', 'danger', 'primary', 'important', 'secondary', '']),
-    options : React.PropTypes.array,
-    children : React.PropTypes.array,
+    type:  React.PropTypes.oneOf([
+      'success', 'danger', 'primary', 'important', 'secondary', ''
+    ]),
+    options:  React.PropTypes.array,
+    children: React.PropTypes.array
   },
-  render : function(){
-    return (
-      <div className={"button-dropdown " + this._isDropdownOpen()} ref="buttonDropdown">
-        <button ref="button" className={this.props.type} onClick={this._toggleDropdownOptions}>
-          <span ref="title">{this.props.title}</span>
-          <span ref="icon" className="arrow ic ic-chevron-down"></span>
-        </button>
-        <ul ref="options" className="options">
-          {this._buildDropdown()}
-        </ul>
-      </div>
-    );
+
+  render: function() {
+    return <div className={'button-dropdown ' + this._isDropdownOpen()} ref='buttonDropdown'>
+      <button className={this.props.type} onClick={this._toggleDropdownOptions} ref='button'>
+        <span ref='title'>{this.props.title}</span>
+        <span className='arrow ic ic-chevron-down' ref='icon'></span>
+      </button>
+      <ul className='options' ref='options'>
+        {this._buildDropdown()}
+      </ul>
+    </div>;
   },
-  _toggleDropdownOptions: function(event){
-    this.setState({ isDropdownOpen : !this.state.isDropdownOpen });
+
+  _toggleDropdownOptions: function(event) {
+    this.setState({
+      isDropdownOpen: !this.state.isDropdownOpen
+    });
   },
-  _isDropdownOpen : function(){
-    return this.state.isDropdownOpen ? "show" : "";
+
+  _isDropdownOpen: function() {
+    return this.state.isDropdownOpen
+      ? 'show'
+      : '';
   },
-  _buildDropdown : function(){
+
+  _buildDropdown: function() {
     return this._populateOptions().map(this._buildOption);
   },
-  _populateOptions : function(){
-    return this.props.children.length > 0 ? this.props.children : this.props.options;
+
+  _populateOptions: function() {
+    return this.props.children.length > 0
+      ? this.props.children
+      : this.props.options;
   },
-  _buildOption : function(option, index){
-    return (<li className="option" onClick={this._handleChange} key={"option-" + index}>{option}</li>);
+
+  _buildOption: function(option, index) {
+    return <li className='option' key={'option-' + index} onClick={this._handleChange}>{option}</li>;
   },
-  _handleChange : function(key){
+
+  _handleChange: function(key) {
     this.props.onChange(key);
   }
 });
