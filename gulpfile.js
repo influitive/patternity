255a2b984eae5856abf8aca5f919841168b2a810
Shane Keulen <shane.keulen@gmail.com>
2015-10-05 10:12:05 -0400

    remove generated fonts from the repo. add font generation to npm prepublish
    this should stop binary merge conflicts on these files

--- gulpfile.js
+++ gulpfile.js
@@ -16,29 +16,25 @@ function adustIconNames(codepoints) {
   });
 }
 
-gulp.task('influicons', function(){
-
+gulp.task('influicons', function() {
   // use SVG files from this directory
   gulp.src(['infl-icons/SVG/*.svg'])
-
     // build the woff, ttf, svg, eot font files named "influicons"
     .pipe(iconfont({
-      fontName: 'influicons',
-      fixedWidth: true,
-      centerHorizontally : true,
-      normalize: true
+      fontName:           'influicons',
+      fixedWidth:         true,
+      centerHorizontally: true,
+      normalize:          true
     }))
-
     .on('codepoints', function(codepoints, options) {
-
       codepoints = adustIconNames(codepoints);
 
       // generate a static CSS demo file
       gulp.src('infl-icons/templates/influicons.css')
         .pipe(consolidate('lodash', {
-          glyphs: codepoints,
-          fontName: 'influicons',
-          fontPath: 'infl-fonts/',
+          glyphs:    codepoints,
+          fontName:  'influicons',
+          fontPath:  'infl-fonts/',
           className: 'ic'
         }))
         .pipe(gulp.dest('infl-fonts/'));
@@ -46,9 +42,9 @@ gulp.task('influicons', function(){
       // generate a static HTML demo file
       gulp.src('infl-icons/templates/influicons.html')
         .pipe(consolidate('lodash', {
-          glyphs: codepoints,
-          fontName: 'influicons',
-          fontPath: 'infl-fonts/',
+          glyphs:    codepoints,
+          fontName:  'influicons',
+          fontPath:  'infl-fonts/',
           className: 'ic'
         }))
         .pipe(gulp.dest('infl-fonts/'));
@@ -56,9 +52,9 @@ gulp.task('influicons', function(){
       // generate the patternity "ic" sass file
       gulp.src('infl-icons/templates/_icon.scss')
         .pipe(consolidate('lodash', {
-          glyphs: codepoints,
-          fontName: 'influicons',
-          fontPath: 'infl-fonts/',
+          glyphs:    codepoints,
+          fontName:  'influicons',
+          fontPath:  'infl-fonts/',
           className: 'ic'
         }))
         .pipe(gulp.dest('infl-styles/'));
@@ -66,14 +62,12 @@ gulp.task('influicons', function(){
       // generate the patternity influicons-list.js file used by "icons_pattern.jsx"
       gulp.src('infl-icons/templates/influicons-list.js')
         .pipe(consolidate('lodash', {
-          glyphs: codepoints,
-          fontName: 'influicons',
-          fontPath: 'infl-fonts/',
+          glyphs:    codepoints,
+          fontName:  'influicons',
+          fontPath:  'infl-fonts/',
           className: 'ic'
         }))
         .pipe(gulp.dest('infl-patternlab/source/js/patterns/atoms/'));
-
-
     })
     .pipe(gulp.dest('infl-fonts/'));
 });
