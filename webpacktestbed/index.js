5488fb0fe8f5ba381a65a7a0d3085f4baee7fd29
Shane Keulen <shane.keulen@gmail.com>
2015-10-07 11:20:32 -0400

    acccordion es6 and animation fix

--- webpacktestbed/index.js
+++ webpacktestbed/index.js
@@ -1,14 +1,14 @@
 import React, { Component } from 'react';
 
 import SaveButton from '../src/save-button';
-import Accordion from '../src/accordion2';
+import Accordion from '../src/accordion';
 
 class App extends Component {
   state = {
     saveStatus: 'unsaved',
     sections: [{
       "header" : "Section Header One",
-      "body" : "Section Body One",
+      "body" : "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
       "key" : "test-2",
       "isEnabled" : true
     },{
