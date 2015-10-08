ab8c6453b6c40b1b1182ad064dcb3218bbf36f4d
Brad Robertson <dev+brad@influitive.com>
2015-10-08 15:03:04 -0400

    allow classnames on button group

--- infl-components/button_group.jsx
+++ infl-components/button_group.jsx
@@ -1,27 +1,36 @@
-var React = require('react');
-var classNames = require('classnames');
+var React      = require('react'),
+    classNames = require('classnames');
 
 var ButtonGroup = React.createClass({
   getDefaultProps: function() {
     return {
-      layout:  'inline',
-      grouped: false
+      layout:    'inline',
+      grouped:   false,
+      className: {}
     };
   },
 
   propTypes: {
-    layout:  React.PropTypes.oneOf(['inline', 'stacked']),
-    grouped: React.PropTypes.bool
+    layout:     React.PropTypes.oneOf(['inline', 'stacked']),
+    grouped:    React.PropTypes.bool,
+    className:  React.PropTypes.oneOfType([
+      React.PropTypes.string,
+      React.PropTypes.object,
+    ])
   },
 
   render: function() {
-    return <div className={'button-group ' + this.props.layout + ' ' + this.isGrouped()} ref='buttonGroup'>
-      {this.props.children}
-    </div>;
-  },
-
-  isGrouped: function() {
-    return this.props.grouped ? 'grouped' : '';
+    return (
+      <div className={this._classNames()} ref='buttonGroup'>
+        {this.props.children}
+      </div>
+    );
+  }
+  _classNames: function () {
+    return classNames(this.props.layout, this.props.className, {
+      "button-group": true,
+      "grouped"     : this.props.grouped
+    });
   }
 });
 
