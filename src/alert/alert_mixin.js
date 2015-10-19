08ffa3bd909f81969f2dffb2278c11b8b32c7d24
Paul Sachs <dev+paul.sachs@influitive.com>
2015-10-19 12:46:30 -0400

    PAT-54:  Made alert title optional.

---
+++ src/alert/alert_mixin.js
@@ -0,0 +1,78 @@
+var React = require('react');
+
+var hideInTimeout;
+
+var AlertMixin = {
+  getInitialState: function() {
+    return {
+      showAlert: this.props.showAlert,
+      closeable: this.props.closeable
+    };
+  },
+
+  componentWillReceiveProps: function(newProps) {
+    this._clearHideInTimeout();
+    this.setState({
+      showAlert: newProps.showAlert,
+      closeable: newProps.closeable
+    });
+  },
+
+  componentDidMount: function() {
+    this._hideAlert(this.props.hideIn);
+  },
+
+  componentWillUpdate: function(nextProps) {
+    this._clearHideInTimeout();
+    this._hideAlert(nextProps.hideIn);
+  },
+
+  _clearHideInTimeout: function() {
+    window.clearTimeout(hideInTimeout);
+  },
+
+  _hideAlert: function(hideIn) {
+    if (hideIn > 0) {
+      hideInTimeout = setTimeout(this._close, this._hideInMilliseconds(hideIn));
+    }
+  },
+
+  _hideInMilliseconds: function(hideIn) {
+    return hideIn * 1000;
+  },
+
+  _showAlert: function() {
+    return this.state.showAlert ? '' : 'hide';
+  },
+
+  _close: function() {
+    this.setState({showAlert: false});
+    this.props.onClose();
+  },
+
+  _icon: function() {
+    return this.props.showIcon
+      ? <span className={'alert-icon ic ' + this._determineIconType()}></span>
+      : '';
+  },
+
+  _hasIconClass: function() {
+    return this.props.showIcon ? 'hasIcon' : '';
+  },
+
+  _determineIconType: function() {
+    return {
+      success: 'ic-check-circle-o',
+      error:   'ic-exclamation-circle-o',
+      warning: 'ic-info-circle-o'
+    }[this.props.type] || 'ic-alert-caution';
+  },
+
+  _closeable: function() {
+    return this.state.closeable
+      ? <span className='close ic ic-times' onClick={this._close} ref='close'></span>
+      : '';
+  }
+};
+
+module.exports = AlertMixin;
---
+++ src/alert/detailed_alert.js
@@ -0,0 +1,66 @@
+var React = require('react');
+var AlertMixin = require('./alert_mixin.js');
+
+var DetailedAlert = {};
+
+var DetailedAlert = React.createClass({
+  mixins: [AlertMixin],
+  getDefaultProps: function() {
+    return {
+      type: "",
+      showIcon: false,
+      closeable : false,
+      showAlert : true,
+      onClose : function(){},
+      hideIn  : 0
+    };
+  },
+  propTypes : {
+    title: React.PropTypes.string,
+    type: React.PropTypes.oneOf(['success', 'error', 'info', 'warning', '']),
+    closeable : React.PropTypes.bool,
+    showAlert : React.PropTypes.bool,
+    onClose: React.PropTypes.func,
+    hideIn: React.PropTypes.number
+  },
+  render : function(){
+    return (
+      <div className={"alert-msg " + this.props.type + " " + this._hasIconClass() + " " + this._showAlert()} ref="alert">
+        {this._closeable()}
+        <h4 className="alert-title" ref="title">
+          {this._icon()}
+          <span>{this.props.title}</span>
+        </h4>
+        <div className="alert-body" ref="body">
+          {this.props.children}
+        </div>
+      </div>
+    );
+  }
+});
+
+DetailedAlert.Detail = React.createClass({
+    getDefaultProps: function() {
+    return {
+      title: "",
+      action: "",
+    };
+  },
+  propTypes : {
+    title: React.PropTypes.string,
+    action: React.PropTypes.node
+  },
+  render : function(){
+    return (
+      <div className="pt-alert-detailed">
+        <h4>{this.props.title}</h4>
+        <span className="pt-alert-detailed-action">{this.props.action}</span>
+        <div className="pt-alert-detailed-body">
+          {this.props.children}
+        </div>
+      </div>
+    );
+  }
+});
+
+module.exports = DetailedAlert;
