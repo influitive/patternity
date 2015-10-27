9d78135227b9ef484f2d89018cdd178aa3ab46c7
Shane Keulen <shane.keulen@gmail.com>
2015-10-27 11:38:01 -0400

    remove default type of primary

--- src/button2/index.js
+++ src/button2/index.js
@@ -23,7 +23,6 @@ class Button extends Component {
   }
 
   static defaultProps = {
-    type:     'primary',
     disabled: false,
     inverse:  false,
     isSubmit: false
@@ -32,32 +31,27 @@ class Button extends Component {
 
   render() {
     const { disabled, onClick, children } = this.props;
-    return <button style={this.props.style} type={this._buttonType()} disabled={this.props.disabled} className={this.getClasses()} onClick={onClick}>
+    return <button style={this.props.style}
+      type={this.isSubmit ? 'submit' : 'button'}
+      disabled={disabled}
+      className={this.getClasses()}
+      onClick={onClick}>
       {children}
     </button>;
   }
 
-
-  _buttonType = () =>{
-    if(this.isSubmit)
-      return 'submit';
-    else
-      return 'button';
-  }
-
   getClasses = () => {
-    const buttonTypes = ['primary', 'secondary', 'important', 'success', 'danger', 'text'];
     const { disabled, inverse, type, children, className, icon } = this.props;
 
     return classNames(
+      'button',
+      type,
+      icon && 'ic ic-' + icon,
       {
-        button:     true,
         disabled:   disabled,
         iconButton: children && children.length === 0
       },
-      icon && 'ic ic-' + icon,
       !disabled && {
-        [type]:  true,
         inverse: (type === 'secondary' || type === 'text') && inverse
       }
     );
