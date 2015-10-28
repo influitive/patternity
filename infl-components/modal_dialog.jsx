348c341985df29c60c9ba7d09c37cb39e605f6a2
Nick Faulkner <nick.faulkner@influitive.com>
2015-10-28 11:11:18 -0400

    stopped event propagation for modal close

--- infl-components/modal_dialog.jsx
+++ infl-components/modal_dialog.jsx
@@ -97,6 +97,8 @@ var ModalDialog = React.createClass({
   },
 
   _closeDialog: function(event) {
+    event.preventDefault();
+    event.stopPropagation();
     if (this.props.closeable && this._isClosableElement(event.target)) {
       this._dismissDialog();
     }
