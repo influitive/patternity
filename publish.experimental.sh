3a5078b4ac048459f8e556e01255fa5dddee8352
Paul Sachs <paul.sachs@influitive.com>
2015-11-03 14:09:26 -0500

    Moving patternlab to it's own repo.
    Patternlab can now be found at https://github.com/influitive/patternlab.

--- publish.experimental.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-
-# AWS_ACCESS_KEY_ID=set-from-circle
-# AWS_SECRET_ACCESS_KEY=set-from-cirle
-BUCKET="patternity.experimental.influitive.com"
-DIR="styleguide/public"
-
-aws s3 sync $DIR s3://$BUCKET/
