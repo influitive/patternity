ab704fb8b7d9cd6a7603644bf0827ff0a547685e
Rohan Nair <rohan@influitive.com>
2015-11-18 15:42:35 -0500

    PAT-127: Re-implemented confirmation and modal components in ES6 React
    Tests are slightly in there, using Tape syntax, but not adequate at the
    moment. The Modal interface has also been broken thanks to ES6 classes
    not allowing namespacing in an intuitive manner.

--- webpacktestbed/index.js
+++ webpacktestbed/index.js
@@ -1,13 +1,38 @@
 import React, { Component } from 'react';
 
-// Confirmation
+// Components
+import Button2 from '../lib/button2';
 import Confirmation from '../src/confirmation';
 
+// Styles
+import '../src/modal_dialog/_modal_dialog.scss';
+import '../src/button2/_button.scss';
+
 class App extends Component {
+  state = {
+    confirmation: false
+  }
+
+  constructor(props) {
+    super(props);
+
+  }
+
   render() {
+    const ConfirmationComponent = this.state.confirmation
+      ? <Confirmation title='ALERT! 'no='Test' onNo={testNo} onYes={this._toggleConfirmation.bind(this)} />
+      : null;
 
     return (
-      <Confirmation title='Testing this stuff 'no='Test' onNo={testNo} onYes={testYes} /> );
+      <div>
+        <Button2 onClick={this._toggleConfirmation.bind(this)}>Show Confirmation!</Button2>
+        { ConfirmationComponent }
+      </div>
+    );
+  }
+
+  _toggleConfirmation() {
+    this.setState({confirmation: !this.state.confirmation});
   }
 }
 
@@ -22,3 +47,4 @@ function testNo(e) {
   e.preventDefault();
   console.log('No pressed');
 }
+
