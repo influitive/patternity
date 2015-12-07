be09ebff5d89a0dff7698621e5779ff5001f1c58
Rohan Nair <rohan@influitive.com>
2015-12-07 13:42:34 -0500

    PAT-141: Checkbox component in ES6 with indeterminate state

--- src/checkbox/checkbox.test.js
+++ src/checkbox/checkbox.test.js
@@ -1,14 +1,27 @@
 import test from 'tape-catch';
 import React from 'react';
 
-import shallow from '../../testUtils/shallow';
-import checkbox from './index.js';
+import { shallow } from 'enzyme';
+import Checkbox from './index.js';
+import sinon from 'sinon';
 
-test('Default modal container renders properly', t => {
+test('<Checkbox />', t => {
 
-  const { instance, result } = shallow(<checkbox />);
+  // This isn't used but leaving it here in case it's useful
+  const changeEv = sinon.spy();
+  const wrapper = shallow(<Checkbox label='ABC123' onChange={changeEv}/>);
 
-  t.equal(result.type, 'span', 'should be a span');
+  t.equal(wrapper.type(), 'span', 'should be a span');
+  t.equal(wrapper.find('.pt-checkbox-label').text(), 'ABC123', 'should match');
+  t.ok(wrapper.filter('span.pt-checkbox'), 'Checkbox exists');
+
+  // Invoke change handler
+  t.notOk(wrapper.state().isChecked, 'Pre-change, not checked');
+
+  // The following test is broken because enyzme
+  // seems to have a problem with it
+  wrapper._handleChange({});
+  t.ok(wrapper.state().isChecked, 'Post-change, is checked')
 
   t.end();
 });
