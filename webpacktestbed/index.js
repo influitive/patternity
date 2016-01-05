bff1aa0068702935e38bc11d57003dabe6512c72
Shane Keulen <shane.keulen@gmail.com>
2016-01-05 09:38:04 -0500

    testbed themeable

--- webpacktestbed/index.js
+++ webpacktestbed/index.js
@@ -1,26 +1,26 @@
 import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 
-import SplitButtonDropdown from '../src/split-button-dropdown';
-import '../infl-styles/_button.scss';
-import '../infl-styles/_icon.scss';
+import { createTheme } from '../src/utils/themeable';
 
-const handleDropdownItemClick = e => { e.preventDefault(); console.log(e); };
+import Button from '../lib/button2';
+
+require('../infl-styles/_button.scss');
+
+const theme = {
+  themeColorPrimary: 'orange'
+};
+
+const Theme = createTheme(theme);
 
 class App extends Component {
   render() {
     return <div>
-      <SplitButtonDropdown title="hello" 
-        onButtonClick={() => console.log('clicked')}>
-        <a href="$">Hell</a>
-        <a href="$">Hell</a>
-        <a href="$">Hell</a>
-        <a href="$">Hell</a>
-        <a href="$">Hell</a>
-      </SplitButtonDropdown>
+      <Button type="primary">Hello There Bob</Button>
+      <Button type="primary">NonTheme</Button>
     </div>;
   }
 }
 
-ReactDOM.render(<App/>, document.getElementById('root'))
+React.render(React.createElement(App), document.getElementById('root'))
 
